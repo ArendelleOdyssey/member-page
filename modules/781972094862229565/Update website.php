4d2d85a9c285bb648e4d364a58e200bc5cecfe3d
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-30 14:23:28 +0200

    Update Update website.php

--- modules/781972094862229565/Update website.php
+++ modules/781972094862229565/Update website.php
@@ -2,20 +2,19 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/includes/module.init.php') ?>
 
 <?php
+    $command = "cd ".$_SERVER['DOCUMENT_ROOT']." && git checkout . && git pull";
     if (isset($_GET['update']) || !empty($_GET['update'])){
-        $command = "cd ".$_SERVER['DOCUMENT_ROOT']." && git checkout . && git pull";
-        // $output = shell_exec($command);
-        // if ($output == null){
-        //     echo "Error: the command was not executed or no output";
-        // } else {
-        //     echo "<pre>$output</pre>";
-        // }
-        echo $command;
+        $output = shell_exec($command);
+        if ($output == null){
+            echo "Error: the command was not executed or no output";
+        } else {
+            echo "<pre>$output</pre>";
+        }
     } else {
         ?>
 <form method="get">
     <p>
-        Click on the button to execute <code>git checkout .</code> and <code>git pull</code> on the server to update the website
+        Click on the button to execute <code><?php echo $command; ?></code> on the server to update the website
     </p>
     <input type="hidden" name="update" value="yes">
     <input type="submit" value="Update now!" style="color: black;">
