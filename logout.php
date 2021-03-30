ceb69a775004fa330cd878d957c20480cb881753
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-30 18:16:36 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
-<?php 
-session_start();
+<?php require_once('config.php');
 session_unset();
-header('Location: .');
+require_once('includes/OAuth2.php');
+$oauth = new OAuth2($website["discord_client"], $website["discord_secret"], $website['url'] . 'login.php');
+$oauth->startRedirection($website["discord_scopes"]);
 ?>
