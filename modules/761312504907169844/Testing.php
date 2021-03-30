09fc64ba1d4837693c627085917d3c266bf2264b
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-30 13:20:00 +0200

    Add test module for imagineers

---
+++ modules/761312504907169844/Testing.php
@@ -0,0 +1,8 @@
+<?php $title = "Testing module"; 
+require_once($_SERVER['DOCUMENT_ROOT'].'/includes/module.init.php') ?>
+
+<p>
+    Hello :p
+</p>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php') ?>
