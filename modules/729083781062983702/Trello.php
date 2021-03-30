20de5adf5f7c144e1141b9631244c38e83335e31
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-30 20:46:03 +0200

    Add trello plugin

---
+++ modules/729083781062983702/Trello.php
@@ -0,0 +1,10 @@
+<?php $title = "Trello Task board"; 
+require_once($_SERVER['DOCUMENT_ROOT'].'/includes/module.init.php') ?>
+
+<p>
+    <a href="https://trello.com/b/wHjJgnkF" target="_blank">Open the Trello</a>
+</p>
+
+<iframe src="https://trello.com/b/wHjJgnkF.html" title="Trello" frameborder="0" width="100%" height="75%"></iframe>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php') ?>
